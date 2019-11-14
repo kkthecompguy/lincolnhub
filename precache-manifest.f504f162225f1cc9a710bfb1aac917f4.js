f0af149e57a48f1a2ea6141656b156d0de2df8a0
Kosam Omollo <hnkhosamomollo3@gmail.com>
2019-11-14 17:15:08 +0300

    Updates

---
+++ precache-manifest.f504f162225f1cc9a710bfb1aac917f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3732c2487f0031a1d4c9de7505a55467",
+    "url": "/lincolnhub/index.html"
+  },
+  {
+    "revision": "0e3154f1a79be3facd4c",
+    "url": "/lincolnhub/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "35eb439aee157cafa5e5",
+    "url": "/lincolnhub/static/css/main.5d15d4ef.chunk.css"
+  },
+  {
+    "revision": "0e3154f1a79be3facd4c",
+    "url": "/lincolnhub/static/js/2.fc26e748.chunk.js"
+  },
+  {
+    "revision": "35eb439aee157cafa5e5",
+    "url": "/lincolnhub/static/js/main.23d9c9a2.chunk.js"
+  },
+  {
+    "revision": "9d1315cad2734494a95b",
+    "url": "/lincolnhub/static/js/runtime-main.9156962b.js"
+  }
+]);
